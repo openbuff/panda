7d83a13384c370ec6b3f99dc26480aced365e888
isaacLyu <isaac@openbuff.org>
2021-01-21 13:51:38 +0800

    Modify nacos config demo

--- nacos-config/src/main/java/org/openbuff/panda/nacosconfig/demo/NacosConfigDemo.java
+++ nacos-config/src/main/java/org/openbuff/panda/nacosconfig/demo/NacosConfigDemo.java
@@ -1,11 +1,14 @@
 package org.openbuff.panda.nacosconfig.demo;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
 
-@Configuration
+@RestController
+@RefreshScope
 public class NacosConfigDemo {
 
     @Value("${user.name}")
@@ -16,6 +19,11 @@ public class NacosConfigDemo {
 
     @PostConstruct
     public void init() {
+        System.out.printf("[init] user name ：%s , age : %d%n", name, age);
+    }
+
+    @RequestMapping("/user")
+    public void user() {
         System.out.printf("[init] user name ：%s , age : %d%n", name, age);
     }
 }
